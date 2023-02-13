643980bac3bd5c392ef076e421cb82417a5f42f3
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2023-02-13 13:42:53 +0100

    chore: upgrade analysis dependency

--- packages/flame_behaviors/lib/flame_behaviors.dart
+++ packages/flame_behaviors/lib/flame_behaviors.dart
@@ -1,3 +1,5 @@
+/// Flame Behaviors applies separation of concerns to game logic in the form of
+/// Entities and Behaviors, built by Very Good Ventures.
 library flame_behaviors;
 
 export 'src/behaviors/behaviors.dart';
